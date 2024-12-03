9217af0526bd6943848d11d691a2e2e068b3f52b
Adrien Morand <90243392+Affy657@users.noreply.github.com>
2024-12-03 17:52:38 +0100

    day 3 bonus

--- Lib/Day03/AlgoDay03.cs
+++ Lib/Day03/AlgoDay03.cs
@@ -6,27 +6,47 @@ namespace Lib
 {
     public class AlgoDay03 : IAlgo
     {
-        public const string PATTERN = @"mul\((\d{1,3}),(\d{1,3})\)";
+        public const string PATTERN_MUL = @"mul\((\d{1,3}),(\d{1,3})\)";
+        public const string PATTERN_DO = @"\bdo\(\)";
+        public const string PATTERN_DONT = @"\bdon't\(\)";
+        public const string PATTERN_ALL_INSTRUCTIONS = @"mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\)";
         public DayAndBonus GetDayAndBonus() => new() { Day = 3 };
 
         public string Solve(string[] input, bool isBonus = false)
         {
-            // input : xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
-            // output : 161
+            // input : xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
+            // output : 48
             int sumOfMul = 0;
+            bool doMul = true;
 
             foreach (string line in input)
             {
-                MatchCollection matches = Regex.Matches(line, PATTERN);
+                MatchCollection matches = Regex.Matches(line,isBonus ? PATTERN_ALL_INSTRUCTIONS : PATTERN_MUL);
 
                 foreach (Match match in matches)
                 {
-                    int x = int.Parse(match.Groups[1].Value);
-                    int y = int.Parse(match.Groups[2].Value);
-                    sumOfMul += x * y;
+                    if (Regex.IsMatch(match.Value, PATTERN_DO) && isBonus)
+                    {
+                        doMul = true;
+                    }
+                    else if (Regex.IsMatch(match.Value, PATTERN_DONT) && isBonus)
+                    {
+                        doMul = false;
+                    }
+                    else if (Regex.IsMatch(match.Value, PATTERN_MUL) && doMul)
+                    {
+                        sumOfMul += Multiply(match);    
+                    }
                 }
             }
             return sumOfMul.ToString();
+        }
+
+        public int Multiply(Match match)
+        {
+            int x = int.Parse(match.Groups[1].Value);
+            int y = int.Parse(match.Groups[2].Value);
+            return x * y;
         }
     }
 }
